562e2cd501744f81ddd3ec08d6042bdeab24effe
esket1 <baykovdr23@st.ithub.ru>
2026-05-31 20:06:55 +0300

    kt done

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 void main() {
   runApp(const MyApp());
@@ -55,6 +56,23 @@ class MyHomePage extends StatefulWidget {
 
 class _MyHomePageState extends State<MyHomePage> {
   int _counter = 0;
+  @override
+  void initState() {
+    super.initState();
+    _loadCounter();
+  }
+
+  void _loadCounter() async {
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    setState(() {
+    _counter = prefs.getInt('counter') ?? 0;
+    });
+  }
+
+  void _saveCounter() async {
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    prefs.setInt('counter', _counter);
+  }
 
   void _incrementCounter() {
     setState(() {
@@ -64,6 +82,7 @@ class _MyHomePageState extends State<MyHomePage> {
       // _counter without calling setState(), then the build method would not be
       // called again, and so nothing would appear to happen.
       _counter++;
+      _saveCounter();
     });
   }
 
